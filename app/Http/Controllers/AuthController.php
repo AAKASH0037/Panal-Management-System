9ee987fd12b36723b2670bfe8a294c7171910ee8
Aakash0037 <aakash@ashmar.in>
2025-12-04 12:00:50 +0530

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    // -------- REGISTER ----------
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name'     => 'required',
+            'email'    => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+            'role'     => 'required|in:admin,seller,buyer' 
+        ]);
+
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => Hash::make($request->password),
+            'role'     => $request->role,   
+        ]);
+
+        return response()->json([
+            'status'  => true,
+            'message' => 'User registered successfully',
+            'data'    => $user
+        ]);
+    }
+
+    // -------- LOGIN ----------
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required'
+        
+        ]);
+
+        $user = User::where('email', $request->email) ->first();
+                         
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['Invalid credentials or role mismatch'],
+            ]);
+        }
+
+        // token generate
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'status'  => true,
+            'message' => 'Login successful',
+            'token'   => $token,
+            'user'    => $user
+        ]);
+    }
+
+    // -------- LOGOUT ----------
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Logged out successfully'
+        ]);
+    }
+}
